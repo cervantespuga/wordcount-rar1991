951b0a9cf1be63901b588b6d2dd67c8e62bec6cd
Cervantes Puga <cervantespuga@protonmail.com>
2020-05-27 15:49:48 +0100

    added postgres db

--- config.py
+++ config.py
@@ -2,12 +2,15 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+print(os.environ["DATABASE_URL"])
+
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = "this-really-needs-to-be-changed"
+    SECRET_KEY = "7f08830c7b63306b0d1502db857935a55151a30620389011be7e7cc1cea8d42f"
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 
 class ProductionConfig(Config):
