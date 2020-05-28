b42beda498a49fbc0a01fa8a517ddbc31877f180
Cervantes Puga <cervantespuga@protonmail.com>
2020-05-28 16:31:57 +0100

    updated mainjs 4th

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('WordcountApp', []).controller('WordcountController', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http,$timeout) {
+    angular.module('WordcountApp', []).controller('WordcountController', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http, $timeout) {
 
         $scope.getResults = function() {
 
@@ -22,28 +22,27 @@
 
         }
         ;
+        function getWordCount(jobID) {
+            var timeout = "";
 
+            var poller = function() {
+                // fire another request
+                $http.get('/results/' + jobID).success(function(data, status, headers, config) {
+                    if (status === 202) {
+                        $log.log(data, status);
+                    } else if (status === 200) {
+                        $log.log(data);
+                        $timeout.cancel(timeout);
+                        return false;
+                    }
+                    // continue to call the poller() function every 2 seconds
+                    // until the timeout is cancelled
+                    timeout = $timeout(poller, 2000);
+                });
+            };
+            poller();
+        }
     }
     ]);
 
 }());
-function getWordCount(jobID) {
-    var timeout = "";
-
-    var poller = function() {
-        // fire another request
-        $http.get('/results/' + jobID).success(function(data, status, headers, config) {
-            if (status === 202) {
-                $log.log(data, status);
-            } else if (status === 200) {
-                $log.log(data);
-                $timeout.cancel(timeout);
-                return false;
-            }
-            // continue to call the poller() function every 2 seconds
-            // until the timeout is cancelled
-            timeout = $timeout(poller, 2000);
-        });
-    };
-    poller();
-}
