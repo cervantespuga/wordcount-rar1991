44faf99dbbf098d94a4a14de6fb05d39882313cc
Cervantes Puga <cervantespuga@protonmail.com>
2020-05-28 16:25:08 +0100

    added basic polling

--- static/main.js
+++ static/main.js
@@ -1,8 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('WordcountApp', [])
-    .controller('WordcountController', ['$scope', '$log', '$http', function($scope, $log, $http) {
+    angular.module('WordcountApp', []).controller('WordcountController', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http) {
 
         $scope.getResults = function() {
 
@@ -16,6 +15,7 @@
                 "url": userInput
             }).success(function(results) {
                 $log.log(results);
+                getWordCount(results);
             }).error(function(error) {
                 $log.log(error);
             });
@@ -27,3 +27,23 @@
     ]);
 
 }());
+function getWordCount(jobID) {
+    var timeout = "";
+
+    var poller = function() {
+        // fire another request
+        $http.get('/results/' + jobID).success(function(data, status, headers, config) {
+            if (status === 202) {
+                $log.log(data, status);
+            } else if (status === 200) {
+                $log.log(data);
+                $timeout.cancel(timeout);
+                return false;
+            }
+            // continue to call the poller() function every 2 seconds
+            // until the timeout is cancelled
+            timeout = $timeout(poller, 2000);
+        });
+    };
+    poller();
+}
