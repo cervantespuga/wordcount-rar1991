b85171c10069cbae7b5f557624b729f360349b8c
Cervantes Puga <cervantespuga@protonmail.com>
2020-05-28 16:35:35 +0100

    updated mainjs 4th

--- static/main.js
+++ static/main.js
@@ -1,48 +1,58 @@
-(function() {
-    'use strict';
+(function () {
 
-    angular.module('WordcountApp', []).controller('WordcountController', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http, $timeout) {
+  'use strict';
 
-        $scope.getResults = function() {
+  angular.module('WordcountApp', [])
 
-            $log.log("test");
+  .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
+    function($scope, $log, $http, $timeout) {
 
-            // get the URL from the input
-            var userInput = $scope.url;
+    $scope.getResults = function() {
 
-            // fire the API request
-            $http.post('/start', {
-                "url": userInput
-            }).success(function(results) {
-                $log.log(results);
-                getWordCount(results);
-            }).error(function(error) {
-                $log.log(error);
-            });
+      $log.log("test");
 
-        }
-        ;
-        function getWordCount(jobID) {
-            var timeout = "";
+      // get the URL from the input
+      var userInput = $scope.url;
 
-            var poller = function() {
-                // fire another request
-                $http.get('/results/' + jobID).success(function(data, status, headers, config) {
-                    if (status === 202) {
-                        $log.log(data, status);
-                    } else if (status === 200) {
-                        $log.log(data);
-                        $timeout.cancel(timeout);
-                        return false;
-                    }
-                    // continue to call the poller() function every 2 seconds
-                    // until the timeout is cancelled
-                    timeout = $timeout(poller, 2000);
-                });
-            };
-            poller();
-        }
+      // fire the API request
+      $http.post('/start', {"url": userInput}).
+        success(function(results) {
+          $log.log(results);
+          getWordCount(results);
+
+        }).
+        error(function(error) {
+          $log.log(error);
+        });
+
+    };
+
+    function getWordCount(jobID) {
+
+      var timeout = "";
+
+      var poller = function() {
+        // fire another request
+        $http.get('/results/'+jobID).
+          success(function(data, status, headers, config) {
+            if(status === 202) {
+              $log.log(data, status);
+            } else if (status === 200){
+              $log.log(data);
+              //$scope.wordcounts = data;
+              //$timeout.cancel(timeout);
+              return false;
+            }
+            // continue to call the poller() function every 2 seconds
+            // until the timeout is cancelled
+            timeout = $timeout(poller, 2000);
+          });
+      };
+      poller();
     }
-    ]);
+
+  }
+  ]);
 
 }());
+
