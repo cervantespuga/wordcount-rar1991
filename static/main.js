774f76d5d675c6f5119922fefd39e27c84a90421
Cervantes Puga <cervantespuga@protonmail.com>
2020-05-28 15:58:03 +0100

    updated backend handler

--- static/main.js
+++ static/main.js
@@ -1,14 +1,23 @@
-(function () {
-  'use strict';
+.controller('WordcountController', ['$scope', '$log', '$http',
+  function($scope, $log, $http) {
 
-  angular.module('WordcountApp', [])
+  $scope.getResults = function() {
 
-  .controller('WordcountController', ['$scope', '$log',
-    function($scope, $log) {
-      $scope.getResults = function() {
-        $log.log("test");
-      };
-    }
-  ]);
+    $log.log("test");
 
-}());
+    // get the URL from the input
+    var userInput = $scope.url;
+
+    // fire the API request
+    $http.post('/start', {"url": userInput}).
+      success(function(results) {
+        $log.log(results);
+      }).
+      error(function(error) {
+        $log.log(error);
+      });
+
+  };
+
+}
+]);
